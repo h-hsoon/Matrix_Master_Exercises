d0604c87bc897fc2b09aca12be73dd5f4b4c8683
unknown <hannahsoon1996@gmail.com>
2024-05-07 16:46:04 +0200

    create js file and the first exercise

---
+++ exercises.js
@@ -0,0 +1,19 @@
+// exercise 1
+// exercise 1.1
+console.log("exercise 1.1");
+var age = 27;
+if (age >= 18){
+    console.log("You are an adult and you can vote");
+} else{
+    console.log("You are not an adult and you cannot vote");
+}
+
+// exercise 1.2
+console.log("exercise 1.2");
+const user_name = prompt("Please enter your name: ");
+user_name = user_name.toUpperCase;
+if (user_name == "JOHN") {
+    console.log("Hello ", user_name);
+} else{
+    console.log("You are not John");
+}
